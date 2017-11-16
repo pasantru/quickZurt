b7b4082178df278f7aef7239a380915ffe517e41
Pasantru <pasantru@gmail.com>
2017-11-16 22:35:46 +0100

    Changed BuscaElem.java

--- src/BuscaElem.java
+++ src/BuscaElem.java
@@ -6,24 +6,28 @@
 import java.util.Scanner;
 
 public final class BuscaElem{
-	
+
 	public static <T extends Comparable<? super T>> T kesimo(T v[], int k) {
 		return kesimoRec(v,0,v.length-1,k);
 	}
 
 	public static <T extends Comparable<? super T>> T kesimoRec(T v[], int izq, int der, int k) {
-		// A IMPLEMENTAR POR EL ALUMNO
-		return v[0];
+		int piv = OrdenacionRapida.partir(v, v[der], izq, der);
+		while(s!=k){
+			if(k>s) piv = OrdenacionRapida.partir(v, v[der], piv, der);
+			else piv = OrdenacionRapida.partir(v, v[piv-1], izq, piv-1);
+		}
+		return v[piv];
     }
-	
-	
+
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int maxvector;
 		int i,k;
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Introduce el numero de posiciones del vector: ");
 		maxvector=sc.nextInt();
